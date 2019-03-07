df793f39eb50d955a847802bb86d4a8106c5daf5
Phillip Lorenzo <thenewphilliplorenzo@outlook.com>
2019-03-07 09:53:38 -0800

    Proxy tests pass (#16)
    * all proxy tests pass

--- test/XyGovernanceProxy.test.js
+++ test/XyGovernanceProxy.test.js
@@ -4,25 +4,19 @@ const { Contracts, ZWeb3 } = require(`zos-lib`)
 ZWeb3.initialize(web3.currentProvider)
 
 const Governance = Contracts.getFromLocal(`XyGovernance`)
-const ERC20 = artifacts.require(`XyERC20Token.sol`)
+const ERC20 = Contracts.getFromNodeModules(`openzeppelin-eth`, `ERC20`)
 
 require(`chai`).should()
 
-contract(`XyGovernanceProxy`, () => {
+contract(`XyGovernance`, () => {
   let project
   beforeEach(async () => {
     project = await TestHelper()
   })
 
-  it(`should create a proxy`, async () => {
+  it(`should create a proxy for the EVM Package`, async () => {
     const proxy = await project.createProxy(Governance)
-    const result = await proxy.methods.proposeNewAction(1, 50, 0).call()
-    result.should.eq(true)
-  })
-
-  it(`should create a proxy for the EVM package`, async () => {
-    const proxy = await project.createProxy(ERC20, { contractName: `XyGovernance`, packageName: `openzeppelin-eth` })
-    const result = await proxy.methods.proposeNewAction(1, 50, 0).call()
-    result.should.eq(true)
+    const result = await proxy.methods.numResolutions(`1`).call()
+    result.should.eq(`0`)
   })
 })
