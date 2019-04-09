7cad5422ed0639536ba287b53469b2515885cc6f
Kevin Weiler <kevinw@gmail.com>
2019-04-09 11:34:01 -0700

    specify gas in test helper

--- test/ProxyXyStakingConsensus.test.js
+++ test/ProxyXyStakingConsensus.test.js
@@ -11,7 +11,7 @@ require(`chai`).should()
 contract(`ProxyXyStakingConsensus`, () => {
   let project
   beforeEach(async () => {
-    project = await TestHelper()
+    project = await TestHelper({ gas: 6721975 })
   })
 
   it(`should create a proxy for the EVM Package`, async () => {
@@ -21,8 +21,7 @@ contract(`ProxyXyStakingConsensus`, () => {
         `0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1`,
         `0x9561c133dd8580860b6b7e504bc5aa500f0f06a7`,
         `0x9561c133dd8580860b6b7e504bc5aa500f0f06a7`
-      ],
-      gas: 6721975
+      ]
     })
     const result = await proxy.methods.getLatestBlock().call()
     result.should.eq(`0x0000000000000000000000000000000000000000000000000000000000000000`)
